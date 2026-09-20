1011e7af67e0cb0642d14cd21732b6384b0365b4
wh00 is 007 <wh00is007@gmail.com>
2026-09-20 09:52:59 -0500

    added required_version ~> 1.7 to terraform block

--- 03-first-tf-project/providers.tf
+++ 03-first-tf-project/providers.tf
@@ -1,15 +1,15 @@
 terraform {
-   required_providers {
-     aws = {
-       source  = "hashicorp/aws"
-       version = "~> 5.0"
-     }
-
-     random = {
-       source  = "hashicorp/random"
-       version = "~> 3.0"
-     }
-   }
+  required_version = "~> 1.7"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
 }
 
 provider "aws" {
